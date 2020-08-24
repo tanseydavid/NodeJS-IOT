8a7bb3c7f676a6af1d0c8ced40a8b13d1e9568db
David Tansey <dtanseymarin@gmail.com>
2020-08-24 16:35:05 -0700

    Fix bad commit

--- connection.js
+++ connection.js
@@ -1,8 +1,8 @@
 const Database = require('./Database.js');
 const db = new Database({
-     host: 'demofuelathome.mysql.database.azure.com',
-     user: 'admin_fuelathome@demofuelathome',
-     password: 'just4NodeJS,',
+     host: 'localhost',
+     user: '***',
+     password: '****',
      database: 'classicmodels'
 });
 module.exports = db;
