814e18a4ba08d663bf92ee944b8686f6d842567d
David Tansey <dtansey@designmind.com>
2020-12-14 15:51:44 -0800

    changes to mysql default port address

--- connection.js
+++ connection.js
@@ -1,6 +1,7 @@
 const Database = require('./Database.js');
 const db = new Database({
      host: process.env.DB_HOST,
+     port: 8306,
      user: process.env.DB_USERNAME,
      password: process.env.DB_PASS,
      database: 'classicmodels'
