f171f4b2db19b4cdf0d953a8bd44a60eba77d277
David Tansey <dtansey@designmind.com>
2020-12-14 16:00:22 -0800

    changes to mysql url to 0.0.0.0

--- connection.js
+++ connection.js
@@ -1,6 +1,6 @@
 const Database = require('./Database.js');
 const db = new Database({
-     host: process.env.DB_HOST,
+     host: '0.0.0.0',
      port: 8306,
      user: process.env.DB_USERNAME,
      password: process.env.DB_PASS,
