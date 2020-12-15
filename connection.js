34891ea96032425cceaf15541144c97ff5654f53
David Tansey <dtansey@designmind.com>
2020-12-14 16:12:34 -0800

    changes to mysql port back to 3306 with 8306 as port exposed to WIN

--- connection.js
+++ connection.js
@@ -1,7 +1,6 @@
 const Database = require('./Database.js');
 const db = new Database({
      host: '0.0.0.0',
-     port: 8306,
      user: process.env.DB_USERNAME,
      password: process.env.DB_PASS,
      database: 'classicmodels'
